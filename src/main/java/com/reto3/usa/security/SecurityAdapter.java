d8b38fe2b27b97f6867a79e3a524cf7652d50dc8
mfernandasosa <zoza.mariaf@gmail.com>
2022-10-16 16:41:31 -0500

    ActualizacionII

---
+++ src/main/java/com/reto3/usa/security/SecurityAdapter.java
@@ -0,0 +1,26 @@
+package com.reto3.usa.security;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+public class SecurityAdapter extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(a -> a.antMatchers("/","/url","/error", "/webjars/**","/Admin/**","/Cabin",
+                "/Category/*", "/Client/", "/Message/", "/Reservation/*",
+                "/Score/**","/api/**").permitAll()
+                .anyRequest().authenticated()
+        ).exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+        ).oauth2Login();
+        http.cors().and().csrf().disable();
+    }
+}
+
+
+
+
